8a3df499c2dacbc88fe03a066571dcbb129aaba6
ToniByte <ToniByte@proton.me>
2026-08-20 15:40:55 +0300

    feat: add S3 backend for Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "tonibyte-tfstate"
+    key    = "gitops/terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
