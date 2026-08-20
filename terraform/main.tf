c5414968f988a9ed94ac3933bb2aa5eab2ee1959
ToniByte <ToniByte@proton.me>
2026-08-20 18:07:45 +0300

    feat: add RDS PostgreSQL and Secrets Manager

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
   backend "s3" {
     bucket = "tonibyte-tfstate-089244386830-eu-central-1-an"
@@ -111,4 +115,92 @@ output "public_ip" {
 
 output "instance_id" {
   value = aws_instance.gitops.id
+}
+
+############################
+# RDS PostgreSQL
+############################
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "random_password" "db" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_db_subnet_group" "app" {
+  name       = "tonibyte-db-subnet"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+resource "aws_security_group" "rds" {
+  name        = "tonibyte-rds-sg"
+  description = "PostgreSQL only from app EC2"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description     = "Postgres from app SG"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "app" {
+  identifier        = "tonibyte-app-db"
+  engine            = "postgres"
+  engine_version    = "16"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  storage_encrypted = true
+
+  db_name  = "tonibyte"
+  username = "tonibyte"
+  password = random_password.db.result
+
+  db_subnet_group_name   = aws_db_subnet_group.app.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+
+  publicly_accessible        = false
+  backup_retention_period    = 7
+  deletion_protection        = false
+  skip_final_snapshot        = true
+  auto_minor_version_upgrade = true
+}
+
+resource "aws_secretsmanager_secret" "db" {
+  name = "tonibyte/app/db"
+}
+
+resource "aws_secretsmanager_secret_version" "db" {
+  secret_id = aws_secretsmanager_secret.db.id
+  secret_string = jsonencode({
+    DB_HOST     = aws_db_instance.app.address
+    DB_PORT     = "5432"
+    DB_NAME     = "tonibyte"
+    DB_USER     = "tonibyte"
+    DB_PASSWORD = random_password.db.result
+  })
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.app.address
 }
