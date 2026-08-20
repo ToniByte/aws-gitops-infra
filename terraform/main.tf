2aa62f87d9e720a634a03c5b4dc44cbc2162e83f
ToniByte <ToniByte@proton.me>
2026-08-20 18:09:56 +0300

    fix: set RDS backup_retention_period=0 for free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -180,7 +180,7 @@ resource "aws_db_instance" "app" {
   vpc_security_group_ids = [aws_security_group.rds.id]
 
   publicly_accessible        = false
-  backup_retention_period    = 7
+  backup_retention_period    = 0
   deletion_protection        = false
   skip_final_snapshot        = true
   auto_minor_version_upgrade = true
